b016924c175b04e29f34f4cc69f6d36fac9e1fff
wfogame <raiderattackers@gmail.com>
2025-06-27 21:15:23 -0400

    Basic want command or exit interface

--- Terminal.cpp
+++ Terminal.cpp
@@ -2,16 +2,21 @@
 #include <string>
 using namespace std;
 
-
+bool GameState = true;
 	bool Terminal(string prompt = "none"){
-		if(prompt == "none" || !prompt.rfind(":",0)){
+        if(prompt == "exit" || prompt == "quit"){
+            cout << "Exiting terminal..." << endl;
+            return false;
 
+        }
+		if(prompt == "none" || prompt.rfind(":",0)){
 
+            cout << "Invalid command. Type 'exit' or 'quit' to exit, or type : to type a real command" << endl;
 			return false;
 		}
 		
 		cout << R"(What is your Command:)";	
-
+        cin >> prompt;
 		return true;	
 	}
 
@@ -30,10 +35,27 @@ using namespace std;
    | |/ _ \ '__| '_ ` _ \| | '_ \ / _` | | | | |_ |/ _` | '_ ` _ \ / _ \
    | |  __/ |  | | | | | | | | | | (_| | | | |__| | (_| | | | | | |  __/
    |_|\___|_|  |_| |_| |_|_|_| |_|\__,_|_|  \_____|\__,_|_| |_| |_|\___|)" <<	std::endl;
-
+    cout << "Type 'exit' or 'quit' to exit the terminal:" << endl;
+    cout << "Type ':' to type a real command:" << endl;
+   
 
 	cin >> command;
-	Terminal(command);
+    
+     
+   
+    if(!GameState){
+    cout << "Exiting terminal..." << endl;
+    GameState = false;
+    return 0;
+
+    }else{
+
+   GameState = Terminal(command);
 
 
-	}
+    }
+
+
+
+
+    }
